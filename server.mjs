9983b271c1794508b2ab0d1b001372e7ef79dc27
KevinTran99 <kevintrann99@gmail.com>
2024-05-11 17:53:26 +0200

    Route calls to blockchainRouter

--- server.mjs
+++ server.mjs
@@ -1,9 +1,10 @@
 import express from 'express';
+import blockchainRouter from './routes/blockchain-routes.mjs';
 
 const app = express();
+const PORT = process.argv[2] || process.env.PORT || 5010;
 
 app.use(express.json());
-
-const PORT = process.argv[2] || process.env.PORT || 5010;
+app.use('/api/v1/blockchain', blockchainRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
