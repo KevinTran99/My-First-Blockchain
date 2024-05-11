1dfe0771b9aeb49aaabffb5df7b93d030c65e7b6
KevinTran99 <kevintrann99@gmail.com>
2024-05-11 18:13:01 +0200

    Importing blockchain

--- controllers/blockchain-controller.mjs
+++ controllers/blockchain-controller.mjs
@@ -1,5 +1,7 @@
+import { blockchain } from '../startup.mjs';
+
 const getBlockchain = (req, res, next) => {
-  res.status(200).json({ success: true, data: 'Get Blockchain funkar!' });
+  res.status(200).json({ success: true, data: blockchain });
 };
 
 const createBlock = (req, res, next) => {
