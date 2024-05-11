150d53dcba68384b94c9b1a12071677f55572537
KevinTran99 <kevintrann99@gmail.com>
2024-05-11 22:24:26 +0200

    Route calls to memberRouter

--- server.mjs
+++ server.mjs
@@ -1,10 +1,12 @@
 import express from 'express';
 import blockchainRouter from './routes/blockchain-routes.mjs';
+import memberRouter from './routes/member-routes.mjs';
 
 const app = express();
 const PORT = process.argv[2] || process.env.PORT || 5010;
 
 app.use(express.json());
 app.use('/api/v1/blockchain', blockchainRouter);
+app.use('/api/v1/members', memberRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
