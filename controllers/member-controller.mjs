8aaa25bbc9450c266a229e4d45fe5ab400dec9af
KevinTran99 <kevintrann99@gmail.com>
2024-05-12 00:08:25 +0200

    Added function to add nodes

--- controllers/member-controller.mjs
+++ controllers/member-controller.mjs
@@ -5,3 +5,26 @@ export const listMembers = (req, res, next) => {
     .status(200)
     .json({ success: true, statuscode: 200, data: blockchain.memberNodes });
 };
+
+export const registerNode = (req, res, next) => {
+  const node = req.body;
+
+  if (
+    blockchain.memberNodes.indexOf(node.nodeUrl) === -1 &&
+    blockchain.nodeUrl !== node.nodeUrl
+  ) {
+    blockchain.memberNodes.push(node.nodeUrl);
+
+    res.status(201).json({
+      success: true,
+      statuscode: 201,
+      data: { message: `Node ${node.nodeUrl} is registered` },
+    });
+  } else {
+    res.status(400).json({
+      success: false,
+      statusCode: 400,
+      data: { message: `Node ${node.nodeUrl} is already registered or yours` },
+    });
+  }
+};
